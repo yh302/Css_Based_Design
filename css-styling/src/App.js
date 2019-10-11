63eb2cef86828d2e39bcad877672364b35659fb9
Yakoob Hayat <yakoob.hayat@sky.uk>
2019-10-11 11:56:08 +0100

    added in menubar component

--- css-styling/src/App.js
+++ css-styling/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Menu from './components/menuBar'
 
 
 function App() {
   return (
     <div className="App">
-      <header></header>
+      <header><Menu/></header>
       <content></content>
       <footer></footer>
     </div>
---
+++ css-styling/src/components/menuBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/MenuBar.css'
+
+
+function menuBar() {
+    return(
+        <div className="menuBar">
+            <button className="menuButton">One</button>
+            <button className="menuButton">Two</button>
+            <button className="menuButton">Three</button>
+            <button className="menuButton">Four</button>
+        </div>
+    )
+}
+export default menuBar
