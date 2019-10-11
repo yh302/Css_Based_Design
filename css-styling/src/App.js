bbdb2aee0a2b27f5c0929354e1a84399d4c06b15
Yakoob Hayat <yakoob.hayat@sky.uk>
2019-10-11 11:47:44 +0100

    more re structuring

---
+++ css-styling/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <content></content>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
